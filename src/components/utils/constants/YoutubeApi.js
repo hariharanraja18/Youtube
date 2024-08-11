ec2ecf35d01c59dab0cabe0a645f056868564e8a
hariharanraja18 <143488671+hariharanraja18@users.noreply.github.com>
2024-08-11 23:47:04 +0530

    Cors issue resolved

--- src/components/utils/constants/YoutubeApi.js
+++ src/components/utils/constants/YoutubeApi.js
@@ -1,9 +1,9 @@
 const API_KEY = 'AIzaSyCCIb--KoCwDoPFl8ndNZHmqV4gpkMUC1Q';
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=${API_KEY}`;
 export const YOUTUBE_SUGGEST_API =
-	'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+	'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 
-export const LIVE_CHAT_COUNT= 20;
+export const LIVE_CHAT_COUNT = 20;
 
 export const Comments = [
 	{
