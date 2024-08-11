8319b06d9345defad280cc8d39aeba02be7fc549
hariharanraja18 <143488671+hariharanraja18@users.noreply.github.com>
2024-08-11 22:15:54 +0530

    updated code

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import ChatMessage from './ChatMessage';
 import { useDispatch, useSelector } from 'react-redux';
 import { addMessages } from './utils/ChatSlice';
-import { generateRandomName, makeRandomMessage } from './helper';
 import { Comments } from './utils/constants/YoutubeApi';
 
 const LiveChat = () => {
